efdb48586d3f210e8bbd3da01cbb6f842e1cbeb1
ylin0025 <ylin0025@student.monash.edu>
2018-09-09 13:13:22 +1000

    build diary page

--- FIT5032_Project_EatSmart/Controllers/DiaryController.cs
+++ FIT5032_Project_EatSmart/Controllers/DiaryController.cs
@@ -6,12 +6,68 @@ using System.Web.Mvc;
 
 namespace FIT5032_Project_EatSmart.Controllers
 {
-    [Authorize]
+    
     public class DiaryController : Controller
     {
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            using (DiaryEventEntities dc = new DiaryEventEntities())
+            {
+                var events = dc.Events.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvent(Event e)
+        {
+            var status = false;
+            using (DiaryEventEntities dc = new DiaryEventEntities())
+            {
+                if (e.EventId > 0)
+                {
+                    //Update the event
+                    var v = dc.Events.Where(a => a.EventId == e.EventId).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.Events.Add(e);
+                }
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+            using (DiaryEventEntities dc = new DiaryEventEntities())
+            {
+                var v = dc.Events.Where(a => a.EventId == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Events.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
         }
 
     }
